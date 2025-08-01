5aea0e47ee8c5345193b416d1807c8175776652f
DebbieAUG <69389914+DebbieAUG@users.noreply.github.com>
2025-08-01 05:38:40 +0000

    final updates

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,7 @@ import authMiddleware from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.route("/dashboard").get(authMiddleware, dashboard);
-router.route("/login").post(login);
+router.route("/dashboard").get(authMiddleware, dashboard); // GET
+router.route("/login").post(login); // POST
 
 export default router;
